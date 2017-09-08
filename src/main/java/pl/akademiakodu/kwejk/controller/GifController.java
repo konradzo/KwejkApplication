06cdc8b9822836642c03ea42e2390e55b262094c
Areo <arkadiusz.rogulski@gmail.com>
2017-09-08 11:36:35 +0200

    optimized imports as solution for heroku deployment

--- src/main/java/pl/akademiakodu/kwejk/controller/GifController.java
+++ src/main/java/pl/akademiakodu/kwejk/controller/GifController.java
@@ -1,6 +1,5 @@
 package pl.akademiakodu.kwejk.controller;
 
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
